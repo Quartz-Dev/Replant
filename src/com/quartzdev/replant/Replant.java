80d7071d8351519cb58060e433b12dd59f805295
QarthO <QarthO@gmail.com>
2015-01-04 17:50:47 -0500

    Added plugin.yml and config.yml

--- src/com/quartzdev/replant/Replant.java
+++ src/com/quartzdev/replant/Replant.java
@@ -8,7 +8,6 @@ public class Replant extends JavaPlugin{
 	
 	@Override
 	public void onEnable() {
-		
 		HarvestListener harvestListener = new HarvestListener();
 		this.getServer().getPluginManager().registerEvents(harvestListener, this);
 		
