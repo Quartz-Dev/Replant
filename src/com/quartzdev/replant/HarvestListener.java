c73ea27c2aa9f531d73b4c51c019b014d33b5a78
rockhopper72 <stevenk72@gmail.com>
2015-01-04 16:05:09 -0500

    Project Start

---
+++ src/com/quartzdev/replant/HarvestListener.java
@@ -0,0 +1,7 @@
+package com.quartzdev.replant;
+
+import org.bukkit.event.Listener;
+
+public class HarvestListener implements Listener{
+
+}
---
+++ src/com/quartzdev/replant/Replant.java
@@ -0,0 +1,17 @@
+package com.quartzdev.replant;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Replant extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
