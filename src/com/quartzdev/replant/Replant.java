38b0838bbe7b5461b070ea4afdc96a2ce5778e25
rockhopper72 <stevenk72@gmail.com>
2015-01-04 17:16:00 -0500

    Doesn't listen if disabled

--- src/com/quartzdev/replant/Replant.java
+++ src/com/quartzdev/replant/Replant.java
@@ -1,10 +1,15 @@
 package com.quartzdev.replant;
 
-import org.bukkit.plugin.Plugin;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Replant extends JavaPlugin{
-	Plugin plugin;
+	
+	private static Logger log = Bukkit.getLogger();
+	private final static String PREFIX = "[Replant] ";
 	
 	@Override
 	public void onEnable() {
@@ -12,13 +17,28 @@ public class Replant extends JavaPlugin{
 		Config config = new Config(this.getConfig());
 		
 		HarvestListener harvestListener = new HarvestListener(config);
-		this.getServer().getPluginManager().registerEvents(harvestListener, this);
+		
+		if(config.enabled()) {
+			this.getServer().getPluginManager().registerEvents(harvestListener, this);
+		}else{
+			log("Replant is disabling because it is disabled in the config.");
+		}
 		
 	}
 	
 	@Override
 	public void onDisable() {
 		
+	}
+	
+	public static void log(Level level, String... messages) {
+		for(String message: messages){
+			log.log(level, PREFIX + message);
+		}
+	}
+	
+	public static void log(String... messages){
+		log(Level.INFO, messages);
 	}
 	
 	
