593530f42c07980ae8d3343bf5f3ab0348f0b68b
QarthO <QarthO@gmail.com>
2015-01-05 18:08:08 -0500

    Continued Working on Listeners

--- src/com/quartzdev/replant/HarvestListener.java
+++ src/com/quartzdev/replant/HarvestListener.java
@@ -1,5 +1,9 @@
 package com.quartzdev.replant;
 
+import java.lang.reflect.Array;
+import java.util.List;
+
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -12,9 +16,12 @@ public class HarvestListener implements Listener{
 	public HarvestListener(Config config) {
 		this.config = config;
 	}
+	Material[] crops = config.getCrops();
+	
 	
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onBlockBreak(BlockBreakEvent event) {
+		Material material = event.getBlock().getType();
 		
 	}
 	
