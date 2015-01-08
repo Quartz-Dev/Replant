100f0e38113fa3b9e4f311bb426a9c19fc86b7cf
rockhopper72 <stevenk72@gmail.com>
2015-01-08 18:00:44 -0500

    Revert "Worked on HarvestListener"
    This reverts commit c6f2ae9651d080973a3d88a19a9b979620b6c701.

--- src/com/quartzdev/replant/HarvestListener.java
+++ src/com/quartzdev/replant/HarvestListener.java
@@ -1,67 +1,27 @@
 package com.quartzdev.replant;
 
-import java.util.Collection;
-import java.util.HashMap;
+import java.lang.reflect.Array;
 import java.util.List;
 
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.Plugin;
 
 public class HarvestListener implements Listener{
 	
-	private Config config;
-	private Plugin plugin;
+	private final Config config;
 	
-	public HarvestListener(Config config, Replant replant) {
+	public HarvestListener(Config config) {
 		this.config = config;
-		this.plugin = replant;
 	}
-	List<Material> crops = config.getCrops();
+	Material[] crops = config.getCrops();
 	
-	HashMap<Material, ItemStack> crop_drops = new HashMap<Material, ItemStack>();
-	
-	public void loadDrops(){
-		
-	}
-	
-	public void replant(Block block, Material crop, Collection<Material> drops){
-		ItemStack seed;
-		seed = crop_drops.get(crop);
-		
-		if(seed == null){
-			return;
-		}
-		
-		if(!drops.contains(seed)){
-			return;
-		}
-		
-		drops.remove(seed);
-		
-		block.setType(crop);
-	}
 	
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onBlockBreak(BlockBreakEvent event) {
-		
 		Material material = event.getBlock().getType();
-		Collection<ItemStack> drops = event.getBlock().getDrops();
-		
-		if(!config.enabled()){
-			return;
-		}
-		
-		if(!crops.contains(material)){
-			return;
-		}
-		
-		
 		
 	}
 	
--- src/com/quartzdev/replant/Replant.java
+++ src/com/quartzdev/replant/Replant.java
@@ -32,7 +32,7 @@ public class Replant extends JavaPlugin {
 		
 		ReplantUser.onEnable(config.getDefault());
 		
-		HarvestListener harvestListener = new HarvestListener(config, this);
+		HarvestListener harvestListener = new HarvestListener(config);
 		ReplantCommand replantCommand = new ReplantCommand(config, this);
 		
 		if (config.enabled()) {
