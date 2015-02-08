c22e720502e25241589eec766cc671b64ebdc133
rockhopper72 <stevenk72@gmail.com>
2015-02-08 13:11:11 -0500

    Added Support for Cocoa Beans
    Fully grown cocoa beans will now be replanted with the correct
    direction.

--- src/com/quartzdev/replant/Replacer.java
+++ src/com/quartzdev/replant/Replacer.java
@@ -3,6 +3,7 @@ package com.quartzdev.replant;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.material.CocoaPlant;
 
 public class Replacer implements Runnable {
 	
@@ -14,12 +15,39 @@ public class Replacer implements Runnable {
 		this.toReplace = toReplace;
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Override
 	public void run() {
 		if (block.getType().equals(Material.AIR)) {
+			
+			BlockFace placeDir = null;
+			
 			block.setType(toReplace);
 			block.getState().update(true, true);
 			block.getRelative(BlockFace.DOWN).getState().update(true);
+			
+			if (block.getState().getData() instanceof CocoaPlant) {
+				BlockFace[] faces = { BlockFace.EAST, BlockFace.WEST, BlockFace.NORTH, BlockFace.SOUTH };
+				CocoaPlant cocoa = (CocoaPlant) block.getState().getData();
+				
+				for (BlockFace face : faces) {
+					Block facing = block.getRelative(face);
+					if (facing.getType().equals(Material.LOG)) {
+						int log = facing.getData() % 4;
+						if (log == 3) {
+							placeDir = face;
+							cocoa.setFacingDirection(placeDir);
+							block.setData(cocoa.getData(), true);
+							
+							break;
+						}
+					}
+				}
+				
+				if (placeDir == null) {
+					block.breakNaturally();
+				}
+			}
 		}
 	}
 	
